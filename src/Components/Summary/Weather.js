5030677944e00bbead16c2e931aa7bf94e566977
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2020-07-05 14:19:53 -0400

    consolelog api call to trpubleshoot.

--- src/Components/Summary/Weather.js
+++ src/Components/Summary/Weather.js
@@ -41,6 +41,7 @@ export default class Weather extends Component {
         const api = process.env.REACT_APP_API_KEY_WEATHER
         const url = `https://api.weatherstack.com/forecast?access_key=${api}&query=${this.state.lat},${this.state.long}`
         let res = await axios(url)
+        console.log(res)
         this.setState({
           location: res.data.location.name,
           temp: res.data.current.feelslike,
